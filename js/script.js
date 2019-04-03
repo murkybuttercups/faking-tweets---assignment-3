61f1381576afeffbb9053c0c7845e6a634c93c6e
Ranaghan <jae.ranaghan@uleth.ca>
2019-04-03 05:12:33 -0600

    created js for profile buttonsgit add -A!

--- js/script.js
+++ js/script.js
@@ -2,10 +2,34 @@ window.onload = init;
 
 function init() {
 
-
+        //donald trump image source: https://twitter.com/realDonaldTrump
         document.getElementById("ft-dtrump").onclick = function() {
-            document.getElementById("#ft-profilepic").src = "images/ft-trumpprofilepic.jpg";
+            document.getElementById("ft-profilepic").src = "images/ft-trumpprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "Donald J. Trump";
+            document.getElementById("ft-handle").innerHTML = "@realDonaldTrump";
         }
+
+        //ariana grande image source: https://twitter.com/ArianaGrande
+        document.getElementById("ft-agrande").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-grandeprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "Ariana Grande";
+            document.getElementById("ft-handle").innerHTML = "@ArianaGrande";
+        }
+
+        //j.k. rowling image source: https://twitter.com/jk_rowling
+        document.getElementById("ft-jkrowling").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-jkrowlingprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "J.K. Rowling";
+            document.getElementById("ft-handle").innerHTML = "@jk_rowling";
+        }
+
+        //wendys image source: https://twitter.com/Wendys
+        document.getElementById("ft-wendys").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-wendysprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "Wendy's";
+            document.getElementById("ft-handle").innerHTML = "@Wendys";
+        }
+
 
 
 	var xhr = new XMLHttpRequest(); 
