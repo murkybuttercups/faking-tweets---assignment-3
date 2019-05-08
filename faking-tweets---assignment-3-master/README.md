ab66dbb2efba57e6f39cc8133d2bc3f0aebfcbd1
murkybuttercups <46687765+murkybuttercups@users.noreply.github.com>
2019-05-08 11:02:49 -0600

    got tweets to work and created media queries

---
+++ faking-tweets---assignment-3-master/README.md
@@ -0,0 +1,33 @@
+# Fake Tweets Project Description
+
+So often while browsing social media, users tend to wonder if they trust their fidelity of their content, and wonder how they can sift through all of the #fakenews. Nowadays with all of the software availiable to us and Twitter becoming a viable source of information, it is easier than ever to fake a tweet. Worry not dear shutins, because this tweet generator just adds to the problem! Fake Tweets by Jason Ranaghan was created to well, create fake tweets. This project aims to poke fun at the absurdity of modern digital communications, and utlizes the Twitter Developer API to do it!
+
+Fake Tweets features four great fake profiles: Donald Trump, Ariana Grande, J.K. Rowling, and Wendy's and limitless possibilities. Popular tweets are randomly generated, so you never know what you're going to get! This site employs HTML, CSS, JavaScript, AJAX & JSON, and PHP. When creating this project, much thought went into accurately recreating Twitter's tweet template and special attention was paid when working with CSS. 
+
+Disclaimer:
+The tweets generated on Fake Tweets are entirely random and do not represent the beliefs or values of any of the fake profiles displays. Please use discretion when using Fake Tweets, or else. :^D
+
+## How to Use Fake Tweets
+
+Click on your most desired fake profile, then click generate. Voila!
+
+## Copyright Information
+All tweets are property of their respective owners!
+
+Preview Images:
+donald trump image source:https://commons.wikimedia.org/wiki/File
+ariana grande image source: https://www.flickr.com/photos/130182513@N07/18407533056
+jk rowling image source: https://commons.wikimedia.org/wiki/File:%C3%8Domha_J.K._Rowling.jpg
+wendys image source: https://www.flickr.com/photos/jeepersmedia/14217730236
+
+Fake Profile Images:
+donald trump image source: https://twitter.com/realDonaldTrump
+ariana grande image source: https://twitter.com/ArianaGrande
+j.k. rowling image source: https://twitter.com/jk_rowling
+wendys image source: https://twitter.com/Wendys
+
+
+
+
+
+
---
+++ faking-tweets---assignment-3-master/js/script.js
@@ -0,0 +1,63 @@
+window.onload = init;
+
+function init() {
+
+    var xhr = new XMLHttpRequest(); 
+
+    xhr.open('GET', 'get_tweets.php', true); 
+
+    xhr.send(null);
+
+    xhr.onload = function() {
+
+        if(xhr.status == 200){
+            var data = JSON.parse(xhr.responseText);
+            console.log(data);
+            tweets = data.statuses; 
+
+            var i = 0;
+            document.getElementById("ft-generate").onclick = function() {
+                var faketweet = tweets[i].text;
+           document.getElementById("ft-tweetcontent").innerText = faketweet;
+           i++;
+
+           if(i >= tweets.length){
+            i = 0;
+           }
+          } 
+        }else {
+                document.getElementById("ft-tweetcontent").innerHTML = xhr.responseText;
+                console.log(xhr);
+        }
+    }
+}
+
+        //donald trump image source: https://twitter.com/realDonaldTrump
+        document.getElementById("ft-dtrump").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-trumpprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "Donald J. Trump";
+            document.getElementById("ft-handle").innerHTML = "@realDonaldTrump";
+        }
+
+        //ariana grande image source: https://twitter.com/ArianaGrande
+        document.getElementById("ft-agrande").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-grandeprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "Ariana Grande";
+            document.getElementById("ft-handle").innerHTML = "@ArianaGrande";
+        }
+
+        //j.k. rowling image source: https://twitter.com/jk_rowling
+        document.getElementById("ft-jkrowling").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-jkrowlingprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "J.K. Rowling";
+            document.getElementById("ft-handle").innerHTML = "@jk_rowling";
+        }
+
+        //wendys image source: https://twitter.com/Wendys
+        document.getElementById("ft-wendys").onclick = function() {
+            document.getElementById("ft-profilepic").src = "images/ft-wendysprofilepic.jpg";
+            document.getElementById("ft-header").innerHTML = "Wendy's";
+            document.getElementById("ft-handle").innerHTML = "@Wendys";
+
+            getTweets();
+}
